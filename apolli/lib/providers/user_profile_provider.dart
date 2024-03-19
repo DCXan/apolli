ab6ac5f584a6b5bee6d3b8d851190f1bf693972c
David Chen <chen.david.1016@gmail.com>
2024-03-19 01:29:54 -0500

    fixed pubspec file

--- apolli/lib/providers/user_profile_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:apolli/models/user.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-class UserProfileNotifier extends StateNotifier {
-  UserProfileNotifier() : super(const User());
-
-  final userProfileProvider =
-      StateNotifierProvider((ref) => UserProfileNotifier());
-}
